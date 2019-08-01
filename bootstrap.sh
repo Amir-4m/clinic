66728184cd9d6bef078e8c00aa290a45700a84d7
Saeed Salehian <saeed@upkook.com>
2019-08-01 23:51:37 +0430

    Changed private git repo due to github US sanctions

--- bootstrap.sh
+++ bootstrap.sh
@@ -37,7 +37,8 @@ sh provision.sh
 sudo python -m pip install --upgrade pip
 sudo pip install --upgrade setuptools wheel virtualenv virtualenvwrapper
 
-ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+ssh-keyscan -t rsa bitbucket.org >> ~/.ssh/known_hosts
+ssh-keyscan -t rsa 45.82.137.40 >> ~/.ssh/known_hosts
 append_to_file "export WORKON_HOME=$HOME/.virtualenvs" $HOME/.bashrc
 append_to_file "export PROJECT_HOME=/vagrant" $HOME/.bashrc
 append_to_file "source /usr/local/bin/virtualenvwrapper.sh" $HOME/.bashrc
