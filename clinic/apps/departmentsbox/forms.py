fc53e2fe282f00b1407f426b0f8a4d297a83364f
Mohsen <moshen212@gmail.com>
2019-09-06 18:23:57 +0430

    ...

--- clinic/apps/departmentsbox/forms.py
+++ clinic/apps/departmentsbox/forms.py
@@ -6,7 +6,7 @@ from .models import Department
 class DepartmentAdminForm(forms.ModelForm):
     class Meta:
         model = Department
-        fields = ['title', 'descriptions', 'logo']
+        fields = ['title', 'descriptions', 'link', 'logo']
 
     def __init__(self, *args, **kwargs):
         super(DepartmentAdminForm, self).__init__(*args, **kwargs)
